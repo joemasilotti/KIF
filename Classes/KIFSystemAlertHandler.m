a46d4404c18d5c7c30fc34dde039bce3a8aefe50
Joe Masilotti <joseph.masilotti@gmail.com>
2014-12-01 07:58:01 -0500

    Add support for dissmising system alerts on iOS 7

--- Classes/KIFSystemAlertHandler.m
+++ Classes/KIFSystemAlertHandler.m
@@ -30,21 +30,31 @@
 
 + (void)acknowledgeSystemAlert {
     // Dynamically link the private UIAutomation framework
-    dlopen([@"/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.1.sdk/Developer/Library/PrivateFrameworks/UIAutomation.framework/UIAutomation" fileSystemRepresentation], RTLD_LOCAL);
+    dlopen([@"/Developer/Library/PrivateFrameworks/UIAutomation.framework/UIAutomation" fileSystemRepresentation], RTLD_LOCAL);
 
     // Directly accessing these class methods cause linker errors
     Class Target = NSClassFromString(@"UIATarget");
     Class NilElement = NSClassFromString(@"UIAElementNil");
 
-    UIATarget *target = [Target localTarget];
-    UIAApplication *app = target.frontMostApp;
-    UIAAlert *alert = app.alert;
-    [[alert.buttons lastObject] tap];
+    // Keep trying until the accessibility server starts up (it takes a little while on iOS 7)
+    UIAApplication *app = nil;
+    while (!app) {
+        @try {
+            [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1f]];
+
+            UIATarget *target = [Target localTarget];
+            app = target.frontMostApp;
+            UIAAlert *alert = app.alert;
+            [[alert.buttons lastObject] tap];
+        }
+        @catch (NSException *exception) { }
+        @finally { }
+    }
 
     // Run until the alert is dismissed
-    do {
+    while (![app.alert isKindOfClass:NilElement]) {
         [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1f]];
-    } while (![app.alert isKindOfClass:[NilElement class]]);
+    }
 }
 
 @end
